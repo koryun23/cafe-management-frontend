d2850c27cb0c4fa55e02abe47b72c285c16cedbd
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-07-26 19:17:32 +0400

    changing manager and waiter icons

--- src/components/ViewUsers.js
+++ src/components/ViewUsers.js
@@ -3,6 +3,11 @@ import React from 'react';
 class ViewUsers extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            users: [
+                
+            ]
+        }
     }
 
     render() {
