4c728e8b45120efacea6c2f249a32cf25326d103
Koryun Grigoryan <koryun.grigoryan.5@gmail.com>
2022-07-28 20:04:59 +0400

    adding assigned tables and ordres items to waiter menu

--- src/components/WaiterMenu.js
+++ src/components/WaiterMenu.js
@@ -4,8 +4,20 @@ import Menu from './Menu.js';
 class WaiterMenu extends React.Component {
     constructor(props) {
         super(props);
-        this.menuItems = []
-        this.dropdownItems = [this.assignedTablesDropdownItems]
+        this.menuItems = [
+            {path: '', text: 'Assigned Tables'},
+            {path: '', text: 'Orders'}
+        ]
+        this.assignedTablesDropdownItems = [
+            {path: '/tables-to-waiters', text: 'View Assigned Tables'}
+        ]
+        this.ordersDropdownItems = [
+            {path: '/orders', text: 'View Orders'}
+        ]
+        this.dropdownItems = [
+            this.assignedTablesDropdownItems,
+            this.ordersDropdownItems
+        ]
     }
 
     render() {
